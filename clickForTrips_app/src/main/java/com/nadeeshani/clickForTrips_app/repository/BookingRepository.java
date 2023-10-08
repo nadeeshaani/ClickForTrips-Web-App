5ed1d1dfd240dde9ab8e8ad4079d31af48716bb5
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-10-08 21:18:23 +0530

    updated

--- clickForTrips_app/src/main/java/com/nadeeshani/clickForTrips_app/repository/BookingRepository.java
+++ clickForTrips_app/src/main/java/com/nadeeshani/clickForTrips_app/repository/BookingRepository.java
@@ -4,5 +4,8 @@ import com.nadeeshani.clickForTrips_app.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long>, JpaSpecificationExecutor<Booking> {
+//    List<Booking> findAllByOrderByBookingId();
 }
