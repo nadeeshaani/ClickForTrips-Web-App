f14f048432b982751ce85b3090278992eb19bca8
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-10-10 03:54:56 +0530

    updated

---
+++ clickForTrips_app/src/main/java/com/nadeeshani/clickForTrips_app/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.nadeeshani.clickForTrips_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService()); // Define your custom userDetailsService here
+
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Define your custom UserDetailsService here for in-memory authentication
+        // You can use InMemoryUserDetailsManager or your custom implementation
+        return new InMemoryUserDetailsManager(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("pass")
+                        .roles("USER")
+                        .build(),
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+}
+
